c6dac0d2a3028c8283412b48638d908f974e00fe
gerbaudo <davide.gerbaudo@gmail.com>
2013-12-05 14:35:16 +0100

    auth stuff

--- env.sh
+++ env.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo "create the chatroom from indico"
+echo "setting up kerberos auth"
+kinit -f -r 25h $USER@CERN.CH
+aklog
+echo "please run crontab -e and add the line below"
+echo "@daily ID=afstoken kinit -R"
+
+echo "please create the chatroom from indico"
 XMPP_PASSWD=""
 XMPP_USERNAME=""
 read    -p "Enter username@jabber.cern.ch : " XMPP_USERNAME
